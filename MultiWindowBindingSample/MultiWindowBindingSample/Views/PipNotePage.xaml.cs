9931613f99cf63f7b8c07907365955db9ee50d09
touseef malik <touseefbsb@gmail.com>
2018-09-18 20:30:12 +0000

    solved the multi window binding sync issue

--- MultiWindowBindingSample/MultiWindowBindingSample/Views/PipNotePage.xaml.cs
+++ MultiWindowBindingSample/MultiWindowBindingSample/Views/PipNotePage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using Microsoft.Toolkit.Uwp.Helpers;
 using MultiWindowBindingSample.Models;
 using MultiWindowBindingSample.Services;
 using MultiWindowBindingSample.ViewModels;
+using Windows.ApplicationModel.Core;
 using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
@@ -16,20 +18,42 @@ namespace MultiWindowBindingSample.Views
         {
             InitializeComponent();
         }
-        
-        private ViewLifetimeControl _viewLifetimeControl;
 
+        private ViewLifetimeControl _viewLifetimeControl;
+        private TextBox parentBox;
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
-            var parameter = e.Parameter as Tuple<Note, ViewLifetimeControl>;
+            var parameter = e.Parameter as Tuple<Note, ViewLifetimeControl, TextBox>;
             ViewModel.FillMyNote(parameter.Item1);
             _viewLifetimeControl = parameter.Item2;
+            parentBox = parameter.Item3;
+            parentBox.TextChanged += ParentBox_TextChanged;
+            PipBox.TextChanged += PipBox_TextChanged; 
             _viewLifetimeControl.Released += OnViewLifetimeControlReleased;
         }
-
+        private async void PipBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string text = PipBox.Text;
+            await CoreApplication.MainView.Dispatcher.AwaitableRunAsync(() =>
+            {
+                if (parentBox.Text != text)
+                    parentBox.Text = text;
+            });
+        }
+        private async void ParentBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string text = parentBox.Text;
+            await _viewLifetimeControl.Dispatcher.AwaitableRunAsync(() =>
+            {
+                if (ViewModel.MyNote.Description != text)
+                    ViewModel.MyNote.Description = text;
+            });
+        }
         private async void OnViewLifetimeControlReleased(object sender, EventArgs e)
         {
+            parentBox.TextChanged -= ParentBox_TextChanged;
+            PipBox.TextChanged -= PipBox_TextChanged;
             _viewLifetimeControl.Released -= OnViewLifetimeControlReleased;
             await WindowManagerService.Current.MainDispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
             {
